73d926ca733b76dac10a924612a79b701838be88
snaatak-deepak <deepak.kushwaha.snaatak@mygurukulam.co>
2025-09-02 09:45:40 +0530

    Update buildAmi.groovy

--- vars/buildAmi.groovy
+++ vars/buildAmi.groovy
@@ -1,33 +1,25 @@
 def call(Map config = [:]) {
-    pipeline {
-        agent any
+    node {
+        stage('Checkout') {
+            checkout([$class: 'GitSCM',
+                branches: [[name: config.branch ?: '*/main']],
+                userRemoteConfigs: [[url: config.repo ?: 'https://github.com/snaatak-deepak/Scripted-Pipeline-AMI.git']]
+            ])
+        }
 
-        stages {
-            stage('Checkout') {
-                steps {
-                    git branch: (config.branch ?: 'main'),
-                        url: (config.repo ?: 'https://github.com/snaatak-deepak/Scripted-Pipeline-AMI.git')
-                }
-            }
+        stage('Validate Packer Template') {
+            sh "packer validate template.json"
+        }
 
-            stage('Validate Packer Template') {
-                steps {
-                    sh "packer validate template.json"
-                }
-            }
+        stage('Build AMI') {
+            script {
+                def region = config.region ?: 'us-east-1'
+                def creds  = config.credentialsId ?: 'aws-cred'
 
-            stage('Build AMI') {
-                steps {
-                    script {
-                        def region = config.region ?: 'us-east-1'
-                        def creds = config.credentialsId ?: 'aws-cred'
-
-                        withEnv(["AWS_REGION=${region}"]) {
-                            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
-                                              credentialsId: creds]]) {
-                                sh "packer build -var 'aws_region=${region}' template.json"
-                            }
-                        }
+                withEnv(["AWS_REGION=${region}"]) {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+                                      credentialsId: creds]]) {
+                        sh "packer build -var 'aws_region=${region}' template.json"
                     }
                 }
             }
